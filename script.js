599a27f025d1d78d57e33bf97a7925618b524625
ArtemSpiz <artemijspizhav@gmail.com>
2023-11-16 20:57:21 +0200

    gg

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const images = document.querySelectorAll('.bg1');
 const controlls = document.querySelectorAll('.controlls');
+const elipse = document.querySelectorAll('.elipse');
+
+
+
 let imageIndex = 0;
+
+
 
 function show(index) {
     images[imageIndex].classList.remove('active');
@@ -10,7 +16,7 @@ function show(index) {
 
 controlls.forEach((e) =>{
     e.addEventListener('click', () => {
-        if (event.target.classList.contains('left')){
+        if (event.target.classList.contains('left')){ 
             let index = imageIndex - 1;
             
             if (index < 0) {
@@ -31,7 +37,28 @@ controlls.forEach((e) =>{
         }
         
     })
-})
+});
 
 
 show(imageIndex);
+
+
+const burger = document.querySelector('#burger');
+const menu = document.querySelector('#menu');
+const close = document.querySelector('#close');
+const frame = document.querySelector('#frame');
+
+
+burger.addEventListener('click', () =>{
+    menu.classList.toggle('disp');
+});
+
+frame.addEventListener('click', () =>{
+    close.classList.toggle('disp1');
+    frame.classList.add('disp');
+});
+
+close.addEventListener('click', () =>{
+    close.classList.add('disp1');
+    frame.classList.toggle('disp');
+});
